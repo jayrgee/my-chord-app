25ef220d167fab083d8182be600c78aad556d80f
jayrgee <jayrgee@fastmail.fm>
2019-06-10 13:02:33 +1000

    move chordChart to json file

--- src/mickeyb.js
+++ src/mickeyb.js
@@ -1,260 +1,6 @@
-/* global vexchords */
 /* eslint-disable prefer-destructuring, comma-dangle  */
 import { ChordBox } from 'vexchords';
-
-const mickeyb = {};
-mickeyb.says = 'h1';
-
-const chordChart = [
-  {
-    section: 'Chord Form Group A (1 through 14)',
-    description: 'Root of the tonic chord is on the 6th string',
-    chords: [
-      {
-        number: 1,
-        name: 'G',
-        chord: [
-          [1, 3, 1],
-          [2, 3, 1],
-          [3, 4, 2],
-          [4, 5, 4],
-          [5, 5, 3],
-          [6, 3, 1]
-        ],
-        position: 0
-      },
-      {
-        number: 2,
-        name: 'Gma7',
-        chord: [[1, 'x'], [2, 3, 2], [3, 4, 4], [4, 4, 3], [5, 'x'], [6, 3, 1]],
-        position: 0
-      },
-      {
-        number: 3,
-        name: 'Gma6',
-        chord: [[1, 'x'], [2, 3, 3], [3, 4, 4], [4, 2, 1], [5, 'x'], [6, 3, 2]],
-        position: 0
-      },
-      {
-        number: 4,
-        name: 'Ami7',
-        chord: [
-          [1, 3, 3],
-          [2, 3, 3],
-          [3, 3, 3],
-          [4, 3, 3],
-          [5, 'x'],
-          [6, 3, 2]
-        ],
-        position: 5,
-        positionText: 2
-      },
-      {
-        number: 5,
-        name: 'Ami6',
-        chord: [
-          [1, 3, 3],
-          [2, 3, 3],
-          [3, 3, 3],
-          [4, 2, 1],
-          [5, 'x'],
-          [6, 3, 2]
-        ],
-        position: 5,
-        positionText: 2
-      },
-      {
-        number: 6,
-        name: 'D13♭5♭9',
-        chord: [
-          [1, 5, 4],
-          [2, 5, 4],
-          [3, 3, 2],
-          [4, 'x'],
-          [5, 4, 3],
-          [6, 2, 1]
-        ],
-        position: 4,
-        positionText: 1
-      },
-      {
-        number: 7,
-        name: 'Gma7',
-        chord: [
-          [1, 5, 4],
-          [2, 5, 4],
-          [3, 2, 2],
-          [4, 'x'],
-          [5, 3, 3],
-          [6, 1, 1]
-        ],
-        position: 3
-      },
-      {
-        number: 8,
-        name: 'Gma7 or Emi9',
-        chord: [[1, 'x'], [2, 5, 4], [3, 5, 3], [4, 3, 1], [5, 'x'], [6, 5, 2]],
-        position: 7,
-        positionText: 4
-      },
-      {
-        number: 9,
-        name: 'A13♭9',
-        chord: [[1, 'x'], [2, 5, 4], [3, 4, 3], [4, 3, 1], [5, 'x'], [6, 4, 2]],
-        position: 6,
-        positionText: 3
-      },
-      {
-        number: 10,
-        name: 'D13',
-        chord: [[1, 5, 4], [2, 3, 2], [3, 3, 2], [4, 2, 1], [5, 'x'], [6, 'x']],
-        position: 7,
-        positionText: 4
-      },
-      {
-        number: 11,
-        name: 'D13♭9',
-        chord: [[1, 5, 4], [2, 2, 1], [3, 3, 2], [4, 2, 1], [5, 'x'], [6, 'x']],
-        position: 7,
-        positionText: 4
-      },
-      {
-        number: 12,
-        name: 'Gma6',
-        chord: [[1, 3, 2], [2, 3, 2], [3, 4, 3], [4, 2, 1], [5, 'x'], [6, 'x']],
-        position: 0
-      },
-      {
-        number: 13,
-        name: 'D11',
-        chord: [[1, 'x'], [2, 3, 1], [3, 5, 4], [4, 5, 3], [5, 'x'], [6, 5, 2]],
-        position: 0
-      },
-      {
-        number: 14,
-        name: 'D7♭5',
-        chord: [[1, 'x'], [2, 3, 1], [3, 5, 4], [4, 4, 3], [5, 'x'], [6, 4, 2]],
-        position: 0
-      }
-    ]
-  },
-  {
-    section: 'Chord Form Group B (15 through 26)',
-    description:
-      'Chords in Group B will have the tonic or root of the key on the 5th string plus their associated chords near that position',
-    chords: [
-      {
-        number: 15,
-        name: 'C',
-        chord: [
-          [1, 'x'],
-          [2, 5, 3],
-          [3, 5, 3],
-          [4, 5, 3],
-          [5, 3, 1],
-          [6, 3, 1]
-        ],
-        position: 0
-      },
-      {
-        number: 16,
-        name: 'Cma7',
-        chord: [[1, 'x'], [2, 5, 4], [3, 4, 2], [4, 5, 3], [5, 3, 1], [6, 'x']],
-        position: 0
-      },
-      {
-        number: 17,
-        name: 'Cma6',
-        chord: [[1, 'x'], [2, 5, 4], [3, 2, 1], [4, 2, 1], [5, 3, 2], [6, 'x']],
-        position: 0
-      },
-      {
-        number: 18,
-        name: 'Dmi7',
-        chord: [[1, 2, 3], [2, 3, 4], [3, 2, 2], [4, 'x'], [5, 2, 1], [6, 'x']],
-        position: 5,
-        positionText: 1
-      },
-      {
-        number: 19,
-        name: 'Dmi6',
-        chord: [[1, 2, 3], [2, 3, 4], [3, 1, 1], [4, 'x'], [5, 2, 2], [6, 'x']],
-        position: 5,
-        positionText: 1
-      },
-      {
-        number: 20,
-        name: 'Dmi7',
-        chord: [[1, 5, 4], [2, 3, 2], [3, 'x'], [4, 4, 3], [5, 2, 1], [6, 'x']],
-        position: 5,
-        positionText: 1
-      },
-      {
-        number: 21,
-        name: 'G7♯5♭9',
-        chord: [
-          [1, 3, 3],
-          [2, 3, 3],
-          [3, 3, 3],
-          [4, 2, 2],
-          [5, 'x'],
-          [6, 2, 1]
-        ],
-        position: 3,
-        positionText: 1
-      },
-      {
-        number: 22,
-        name: 'Cma9',
-        chord: [
-          [1, 2, 3],
-          [2, 2, 3],
-          [3, 1, 1],
-          [4, 1, 1],
-          [5, 2, 2],
-          [6, 'x']
-        ],
-        position: 3,
-        positionText: 1
-      },
-      {
-        number: 23,
-        name: 'G13',
-        chord: [
-          [1, 4, 4],
-          [2, 4, 4],
-          [3, 3, 3],
-          [4, 2, 2],
-          [5, 'x'],
-          [6, 2, 1]
-        ],
-        position: 3,
-        positionText: 1
-      },
-      {
-        number: 24,
-        name: 'G7♭5',
-        chord: [[1, 'x'], [2, 5, 4], [3, 3, 2], [4, 'x'], [5, 3, 1], [6, 'x']],
-        position: 4,
-        positionText: 2
-      },
-      {
-        number: 25,
-        name: 'G7',
-        chord: [[1, 'x'], [2, 2, 3], [3, 3, 3], [4, 2, 2], [5, 'x'], [6, 2, 1]],
-        position: 3,
-        positionText: 1
-      },
-      {
-        number: 26,
-        name: 'G♯dim',
-        chord: [[1, 'x'], [2, 2, 1], [3, 3, 3], [4, 2, 1], [5, 'x'], [6, 3, 2]],
-        position: 4,
-        positionText: 2
-      }
-    ]
-  }
-];
+import chordChart from './mickeyb.json';
 
 const chords = [];
 
@@ -270,9 +16,12 @@ function createChordElement(chordStruct) {
   const position = chordStruct.position || 0;
   const positionText = chordStruct.positionText || 0;
   // eslint-disable-next-line no-nested-ternary
-  const offset = (positionText > 0)
-    ? position - positionText - 1
-    : position > 0 ? position - 1 : position;
+  const offset =
+    positionText > 0
+      ? position - positionText - 1
+      : position > 0
+      ? position - 1
+      : position;
 
   const newChord = {
     el: chordcanvas[0],
@@ -280,7 +29,7 @@ function createChordElement(chordStruct) {
     frets: chordStruct.chord
       .map(x => x[1]) // assumes chord string order is 1,2,3,4,5,6
       .reverse()
-      .map(x => ((x === 'x') ? x : x + offset))
+      .map(x => (x === 'x' ? x : x + offset))
   };
 
   chords.push(newChord);
@@ -314,7 +63,7 @@ function createSectionElement(sectionStruct) {
 function init() {
   const container = $('#container');
 
-  // Display preset chords (open chords)
+  // Display chordChart
   for (let i = 0; i < chordChart.length; i += 1) {
     const sectionStruct = chordChart[i];
     const section = createSectionElement(sectionStruct);
@@ -327,7 +76,7 @@ function init() {
   }
 
   // Render chords
-  chords.forEach((chord) => {
+  chords.forEach(chord => {
     const struct = chord.struct;
     const numFrets = struct.numFrets || 5;
     new ChordBox(chord.el, {
@@ -339,4 +88,6 @@ function init() {
   });
 }
 
-$(() => { init(); });
+(() => {
+  init();
+})();
